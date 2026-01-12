0a84f82d669f106dd4bd153db4fcdc66b2613bb1
kfischi <kfir.biz@gmail.com>
2026-01-12 14:56:14 +0200

    Simplify content paths and update background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // זה יכסה את כל הקבצים בתוך src
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // לביטחון אם יש קבצים מחוץ ל-src
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#050505",
       },
     },
   },
